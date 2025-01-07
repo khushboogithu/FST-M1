9dc6ef80f3eaf39c614b85e912797315864f1399
khushboogithu <khushboo.khushboo@ibm.com>
2025-01-07 20:16:18 +0530

    Add files via upload

---
+++ Python/Activities/Activity22_test.py
@@ -0,0 +1,31 @@
+import pytest
+import math
+
+@pytest.mark.activity22  
+def test_add():
+   num1 = 25
+   num2 = 25
+   sum = num1 + num2
+   assert sum == 50
+   
+@pytest.mark.activity22   
+def test_substract():
+   num1 = 75
+   num2 = 25
+   substract = num1-num2
+   assert substract == 50
+
+#@pytest.mark.activity22  
+def test_multiply():
+   num1 = 5
+   num2 = 10
+   multiply = num1 * num2
+   assert multiply == 50
+
+#@pytest.mark.activity22   
+def test_quotient():
+   num1 = 25
+   num2 = 25
+   quotient = num1/num2
+   assert quotient == 1         
+
