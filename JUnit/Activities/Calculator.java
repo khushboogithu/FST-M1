ada4f7a7287b7e1f526896822b22e5fa248025be
khushboogithu <khushboo.khushboo@ibm.com>
2025-03-20 23:16:33 +0530

    Add files via upload

---
+++ JUnit/Activities/Calculator.java
@@ -0,0 +1,17 @@
+package examples;
+
+public class Calculator {
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int square(int num) {
+		return num * num;
+
+	}
+}
